586473d73c3d5e54d0e3d7463671a440d9583cba
Zilonis123 <kazaks.karlis2009@gmail.com>
2023-01-27 21:02:59 +0200

    ✅ chat gpt adds functions
    Chat gpt updated the code to have functions and more comments

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 This is chat gpt creating minesweeper with just a little guidance from me. None of the comments are written by me they were written by chat-gpt
 ## Info
 lines written by:
-- chatgpt: *27*
+- chatgpt: *89*
 - me: *0*
+## How do i run this?
+```shell
+py minesweeper.py
+```
--- minesweeper.py
+++ minesweeper.py
@@ -13,32 +13,58 @@ green = (0, 255, 0)
 # Define the bomb symbol
 BOMB = -1
 
-# Create a 8x8 matrix filled with zeros
-matrix = np.zeros((8, 8))
+def generate_matrix():
+    """This function generates an 8x8 matrix filled with zeros and
+    places mines at random locations.
+    """
+    # Create a 8x8 matrix filled with zeros
+    matrix = np.zeros((8, 8))
 
-# Place mines at random locations
-for i in range(10):
-    x, y = np.random.randint(0, 8), np.random.randint(0, 8)
-    if matrix[x][y] == BOMB:
-        i -= 1 # Decrement i so that the loop doesn't skip an iteration
-        continue
-    matrix[x][y] = BOMB # Place a bomb in the matrix
-
-# Count the number of mines in the surrounding cells
-for x in range(8):
-    for y in range(8):
+    # Place mines at random locations
+    for i in range(10):
+        x, y = np.random.randint(0, 8), np.random.randint(0, 8)
         if matrix[x][y] == BOMB:
-            continue # Skip cells that contain a bomb
-        count = 0
-        for dx in range(-1, 2):
-            for dy in range(-1, 2):
-                if dx == 0 and dy == 0:
-                    continue
-                if x + dx < 0 or x + dx >= 8 or y + dy < 0 or y + dy >= 8:
-                    continue
-                if matrix[x + dx][y + dy] == BOMB:
-                    count += 1
-        matrix[x][y] = count
+            i -= 1 # Decrement i so that the loop doesn't skip an iteration
+            continue
+        matrix[x][y] = BOMB # Place a bomb in the matrix
+    return matrix
+
+def count_mines(matrix):
+    """This function counts the number of mines in the surrounding cells
+    of the matrix.
+    """
+    for x in range(8):
+        for y in range(8):
+            if matrix[x][y] == BOMB:
+                continue # Skip cells that contain a bomb
+            count = 0
+            for dx in range(-1, 2):
+                for dy in range(-1, 2):
+                    if dx == 0 and dy == 0:
+                        continue
+                    if x + dx < 0 or x + dx >= 8 or y + dy < 0 or y + dy >= 8:
+                        continue
+                    if matrix[x + dx][y + dy] == BOMB:
+                        count += 1
+            matrix[x][y] = count
+    return matrix
+
+def draw_matrix(matrix):
+    """This function draws the matrix on the screen using pygame.
+    """
+    for x in range(8):
+        for y in range(8):
+            rect = (40 * x, 40 * y, 40, 40)
+            if matrix[x][y] == BOMB:
+                pygame.draw.rect(screen, red, rect)
+            else:
+                pygame.draw.rect(screen, green, rect)
+
+# Generate matrix
+matrix = generate_matrix()
+
+# Count mines
+matrix = count_mines(matrix)
 
 # Game loop
 running = True
@@ -47,12 +73,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     # Draw the matrix
-    for x in range(8):
-        for y in range(8):
-            if matrix[x][y] == BOMB:
-                pygame.draw.rect(screen, red, (40 * x, 40 * y, 40, 40))
-            else:
-                pygame.draw.rect(screen, green, (40 * x, 40 * y, 40, 40))
+    draw_matrix(matrix)
     pygame.display.update()
 
 # Quit pygame
