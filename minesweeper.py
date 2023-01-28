668345c6a2c51b9f0a85a401adf4fb41949e0c44
Zilonis123 <kazaks.karlis2009@gmail.com>
2023-01-28 12:49:14 +0200

    ✅ When the game starts don't show the map until clicked on the game

--- minesweeper.py
+++ minesweeper.py
@@ -4,28 +4,22 @@ from minesweeper_logic import generate_matrix, count_mines
 # Initialize pygame
 pygame.init()
 
-# Define the screen size
+# Define the screen
 screen_size = (320, 320)
-
-# Create the screen with defined size and set caption and icon
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("Minesweeper")
 icon = pygame.image.load("Dalle-thumbnail.png")
 pygame.display.set_icon(icon)
 
-def draw_matrix(matrix, matrix_size):
-    """
-    Draws the matrix on the screen using pygame.
-    Uses different background colors for different numbers.
-    """
-    # Define color dictionary for numbers
-    color_dict = {1: (0, 255, 0), 2: (0, 0, 255), 3: (255, 0, 0), 
-                  4: (0, 255, 255), 5: (255, 0, 255), 6: (255, 255, 0),
-                  7: (128, 0, 128), 8: (128, 128, 0)}
-    
-    # Initialize font
-    font = pygame.font.SysFont("comicsans", 20)
+# Define color dictionary
+color_dict = {1: (0, 255, 0), 2: (0, 0, 255), 3: (255, 0, 0), 
+              4: (0, 255, 255), 5: (255, 0, 255), 6: (255, 255, 0),
+              7: (128, 0, 128), 8: (128, 128, 0)}
 
+# Initialize font
+font = pygame.font.SysFont("comicsans", 20)
+
+def draw_matrix(matrix, matrix_size):
     for x in range(matrix_size):
         for y in range(matrix_size):
             # Look up color for current cell
@@ -41,15 +35,26 @@ def draw_matrix(matrix, matrix_size):
                 text = font.render(str(int(matrix[x][y])), True, (0, 0, 0))
                 screen.blit(text, (40 * x + 15, 40 * y + 10))
 
-
-# main function
+# Create matrix and count mines
 matrix = generate_matrix()
 count_mines(matrix)
+
+# Keep track of whether the game has started
+game_started = False
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            game_started = True
 
-    draw_matrix(matrix, 8)
+    # Clear screen
+    screen.fill((255, 255, 255))
+    
+    # Only draw matrix if the game has started
+    if game_started:
+        draw_matrix(matrix, 8)
+
     pygame.display.update()
+
